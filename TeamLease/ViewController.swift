79b8d9c2744a2f12eb1ff68b46725ff951e83435
mahadev <mahadevw@rupeeseed.com>
2021-06-28 19:42:21 +0530

    Initial Commit

---
+++ TeamLease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamLease
+//
+//  Created by Mahadev on 28/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
